3a4dda2a864a954bed00acff48c7df11d9c07ffc
Zack Pashkin <kaisenaiko@gmail.com>
2021-02-18 14:46:09 +0000

    permutations

---
+++ leetcode/permutations.py
@@ -0,0 +1,16 @@
+# 47. Permutations II
+
+# Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+# Example 1:
+nums = [1,1,2]
+# Output:
+# [[1,1,2],
+#  [1,2,1],
+#  [2,1,1]]
+# Example 2:
+
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+def permutations(nums):
+  print("this is permutation problem")
--- main.py
+++ main.py
@@ -5,8 +5,12 @@ print(os.path.join(os.getcwd(),"leetcode"))
 sys.path.append(os.path.join(os.getcwd(),"leetcode"))
 
 from spiral_matrix import spiral_matrix
+from permutations import permutations
 
 
 # matrix = [[1,2,3],[4,5,6],[7,8,9]]
-matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
-print(spiral_matrix(matrix))
+# matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+# print(spiral_matrix(matrix))
+
+nums = [1,1,2]
+print(permutations(nums))
