b782760c6c1ccec9a4cf3f1e88d6d6144d3ce3f8
Zack Pashkin <kaisenaiko@gmail.com>
2021-02-18 14:01:32 +0000

    spiral matrix

--- leetcode/spiral_matrix.py
+++ leetcode/spiral_matrix.py
@@ -1,10 +1,10 @@
 #Spiral Matrix Leetcode 54 mid
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# matrix = [[1,2,3],[4,5,6],[7,8,9]]
 # Output: [1,2,3,6,9,8,7,4,5]
 # Example 2:
  
 
-# Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 # Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 # first try
@@ -41,13 +41,16 @@ def spiral_matrix(matrix):
     for i in range(t,b):
       res.append(matrix[i][r - 1])
     r -= 1
+    if not (l < r and t < b):
+      break
     # for bottom in reversed order
     for i in range(r-1, l-1,-1):
       res.append(matrix[b-1][i])
-
+    b -= 1
     # for left col form bottom to top in reversed order
     for i in range(b-1, t-1,-1):
-      res.append(matrix[i][0])
-
+      res.append(matrix[i][l])
+    l += 1
+ 
   return res
 
--- main.py
+++ main.py
@@ -7,5 +7,6 @@ sys.path.append(os.path.join(os.getcwd(),"leetcode"))
 from spiral_matrix import spiral_matrix
 
 
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# matrix = [[1,2,3],[4,5,6],[7,8,9]]
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 print(spiral_matrix(matrix))
